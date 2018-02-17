378662f48652c07bab0e46ab8591b9435302ea48
mluerman <36320878+mluerman@users.noreply.github.com>
2018-02-17 14:10:03 -0500

    Update GroundVehicle.py

--- GroundVehicle.py
+++ GroundVehicle.py
@@ -18,6 +18,16 @@ class GroundVehicle:
     """
     def __init__(self, pose, s, omega):
         """Initialization of ground vehicle, invalid parameters result in ValueError"""
+        if type(pose[0]) not in [int, float]:
+            raise TypeError("x-position input must be an integer or float")
+        if type(pose[1]) not in [int, float]:
+            raise TypeError("y-position input must be an integer or float")
+        if type(pose[2]) not in [int, float]:
+            raise TypeError("Theta input must be an integer or float")
+        if type(s) not in [int, float]:
+            raise TypeError("Speed input must be an integer or float")
+        if type(omega) not in [int, float]:
+            raise TypeError("Omega input must be an integer or float")
         if not 0 <= pose[0] <= 100:
             raise ValueError("x-position must be in range [0, 100]")
         if not 0 <= pose[1] <= 100:
@@ -33,6 +43,8 @@ class GroundVehicle:
         self.omega = omega
         self.xdot = self.s * math.cos(self.pose[2])
         self.ydot = self.s * math.sin(self.pose[2])
+        self.setVelocity([self.xdot, self.ydot, self.omega])
+
         # Check that velocity meets the speed constraint
         assert 5.0 <= (self.xdot**2 + self.ydot**2)**.5 <= 10.0
 
@@ -50,19 +62,18 @@ class GroundVehicle:
             raise TypeError("Theta parameter must be an integer or float")
         if -math.pi <= theta <= math.pi:
             return theta
+        elif theta > math.pi:
+            mod_t = theta % (math.pi * 2)
+            if mod_t <= math.pi:
+                return mod_t - math.pi
+            elif mod_t > math.pi:
+                return mod_t - (2 * math.pi)
         else:
-            # Decrease theta by increments of 2 pi until the theta is in range [-pi, pi]
-            if theta > 0:
-                while True:
-                    theta += -2 * math.pi
-                    if theta <= math.pi:
-                        return theta
-            # Increase theta by increments of 2 pi until the theta is in range [-pi, pi]
+            mod_t = theta % (math.pi * 2)
+            if mod_t <= math.pi:
+                return mod_t
             else:
-                while True:
-                    theta += 2 * math.pi
-                    if theta >= -math.pi:
-                        return theta
+                return mod_t - math.pi
 
     def setPosition(self, pose):
         """Sets the vehicle position and orientation. If any input values exceed the constraints,
@@ -104,27 +115,54 @@ class GroundVehicle:
         self.ydot = vel[1]
         self.omega = vel[2]
         # If velocity constraint is exceeded, clamp values
-        if (self.xdot**2 + self.ydot**2)**2 < 5:
+        if (self.xdot**2 + self.ydot**2)**.5 < 5:
             # Determine new theta
-            theta = math.atan(self.ydot/self.xdot)
+            if self.xdot == 0 and self.ydot == 0:
+                theta = self.getPosition()[2]
+            else:
+                if self.xdot == 0 and self.ydot > 0:
+                    theta = math.pi / 2
+                elif self.xdot == 0 and self.ydot < 0:
+                    theta = -math.pi / 2
+                elif self.ydot == 0 and self.xdot > 0:
+                    theta = 0
+                elif self.ydot == 0 and self.xdot < 0:
+                    theta = math.pi
+                else:
+                    theta = math.atan(self.ydot/self.xdot)
             # Calculate new xdot and ydot and set theta
             self.xdot = 5 * math.cos(theta)
             self.ydot = 5 * math.sin(theta)
             self.setPosition([self.pose[0], self.pose[1], theta])
-        elif (self.xdot ** 2 + self.ydot ** 2) ** 2 > 10:
+        elif (self.xdot ** 2 + self.ydot ** 2)**.5 > 10:
             # Determine new theta
-            theta = math.atan(self.ydot / self.xdot)
+            if self.xdot == 0 and self.ydot > 0:
+                theta = math.pi / 2
+            elif self.xdot == 0 and self.ydot < 0:
+                theta = -math.pi / 2
+            elif self.ydot == 0 and self.xdot > 0:
+                theta = 0
+            elif self.ydot == 0 and self.xdot < 0:
+                theta = math.pi
+            else:
+                theta = math.atan(self.ydot / self.xdot)
             # Calculate new xdot and ydot and set theta
             self.xdot = 10 * math.cos(theta)
             self.ydot = 10 * math.sin(theta)
             self.setPosition([self.pose[0], self.pose[1], theta])
-        assert 5.0 <= (self.xdot**2 + self.ydot**2)**.5 <= 10.0
+        else:
+            pass
+        self.s = (self.xdot**2 + self.ydot**2)**.5
+        # Allow epsilon tolerance for small rounding errors in speed calculation
+        eps = .000000000000001
+        assert 5.0 - eps <= self.s <= 10.0 + eps, "speed is not in bounds"
 
     def controlVehicle(self, c):
         """Updates vehicle velocity based on Control class instance"""
         if not isinstance(c, Control):
             raise TypeError("Parameter c must be an instance of the Control class")
         # Calculate new translational velocities based on speed and current pose
+        self.s = c.getSpeed()
         xdot = c.getSpeed() * math.cos(self.getPosition()[2])
         ydot = c.getSpeed() * math.sin(self.getPosition()[2])
         # Use of class method will carry out any necessary clamping
@@ -134,10 +172,10 @@ class GroundVehicle:
         """Updates the pose, speed, and velocity of the vehicle after time t in seconds and milliseconds."""
         if type(sec) not in [int, float]:
             raise TypeError("Seconds parameter must be an integer or float")
-        if sec < 0:
-            raise ValueError("Seconds parameter must be nonzero")
         if type(msec) not in [int, float]:
             raise TypeError("Milliseconds parameter must be an integer or float")
+        if sec < 0:
+            raise ValueError("Seconds parameter must be nonzero")
         if msec < 0:
             raise ValueError("Milliseconds parameter must be nonzero")
         dt = sec + (msec * 0.001)
